656b821505b381f0a1d93f283ea1b7e38fdd040a
RuslanV93 <ruslanvak93@gmail.com>
2024-12-23 17:24:58 +0300

    vercel update2

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,83 +3,103 @@ import SETTINGS from '../settings';
 import { BLOGGERS_PLATFORM } from '../common/variables/variables';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 /** Database */
-export class Database {
-  private client: MongoClient;
-  private mongoUrl: string;
-  constructor(mongoUrl: string) {
-    this.mongoUrl = mongoUrl;
-    this.client = new MongoClient(mongoUrl);
-  }
-  public async runDb() {
-    try {
-      await this.client.connect();
-      console.log('Database Connected');
-      const adminDb = this.client.db().admin();
-      const pingResult = await adminDb.command({ ping: 1 });
-      console.log('Ping successful:', pingResult);
-    } catch (error) {
-      console.log('DB connection failed: ', error);
-      await this.client.close();
-    }
-  }
-  public async stopDB() {
-    await this.client.close();
-    console.log('DB connection closed.');
-  }
+// export class Database {
+//   private client: MongoClient;
+//   private mongoUrl: string;
+//   constructor(mongoUrl: string) {
+//     this.mongoUrl = mongoUrl;
+//     this.client = new MongoClient(mongoUrl);
+//   }
+//   public async runDb() {
+//     try {
+//       await this.client.connect();
+//       console.log('Database Connected');
+//       const adminDb = this.client.db().admin();
+//       const pingResult = await adminDb.command({ ping: 1 });
+//       console.log('Ping successful:', pingResult);
+//     } catch (error) {
+//       console.log('DB connection failed: ', error);
+//       await this.client.close();
+//     }
+//   }
+//   public async stopDB() {
+//     await this.client.close();
+//     console.log('DB connection closed.');
+//   }
+//
+//   /** Database collections */
+//   public get bloggers_platform_db() {
+//     return this.client.db(BLOGGERS_PLATFORM.dbName);
+//   }
+//   public get blogsCollection() {
+//     return this.client
+//       .db(BLOGGERS_PLATFORM.dbName)
+//       .collection(BLOGGERS_PLATFORM.blogs);
+//   }
+//   public get postsCollection() {
+//     return this.client
+//       .db(BLOGGERS_PLATFORM.dbName)
+//       .collection(BLOGGERS_PLATFORM.posts);
+//   }
+//   public get commentsCollection() {
+//     return this.client
+//       .db(BLOGGERS_PLATFORM.dbName)
+//       .collection(BLOGGERS_PLATFORM.comments);
+//   }
+//   public get usersCollection() {
+//     return this.client
+//       .db(BLOGGERS_PLATFORM.dbName)
+//       .collection(BLOGGERS_PLATFORM.users);
+//   }
+// }
+//
+// export const getDbUrl = async () => {
+//   if (process.env.NODE_ENV === 'test') {
+//     const server = await MongoMemoryServer.create();
+//     return server.getUri();
+//   }
+//   return SETTINGS.DB_URL || 'mongodb://0.0.0.0:27017';
+// };
+// // @ts-ignore
+// let bloggers_platform_db: Db;
+// let blogsCollection: Collection;
+// let postsCollection: Collection;
+// let commentsCollection: Collection;
+// let usersCollection: Collection;
+//
+// const db = getDbUrl().then((url) => {
+//   const db = new Database(url);
+//   bloggers_platform_db = db.bloggers_platform_db;
+//   blogsCollection = db.blogsCollection;
+//   postsCollection = db.postsCollection;
+//   commentsCollection = db.commentsCollection;
+//   usersCollection = db.usersCollection;
+// });
+//
+// export {
+//   blogsCollection,
+//   postsCollection,
+//   commentsCollection,
+//   usersCollection,
+//   bloggers_platform_db,
+// };
+const mongoUrl = SETTINGS.DB_URL || 'mongodb://0.0.0.0:27017';
+export const client = new MongoClient(mongoUrl);
+export const db = client.db(BLOGGERS_PLATFORM.dbName);
+export const blogsCollection = db.collection(BLOGGERS_PLATFORM.blogs);
+export const postsCollection = db.collection(BLOGGERS_PLATFORM.posts);
+export const usersCollection = db.collection(BLOGGERS_PLATFORM.users);
+export const commentsCollection = db.collection(BLOGGERS_PLATFORM.comments);
 
-  /** Database collections */
-  public get bloggers_platform_db() {
-    return this.client.db(BLOGGERS_PLATFORM.dbName);
-  }
-  public get blogsCollection() {
-    return this.client
-      .db(BLOGGERS_PLATFORM.dbName)
-      .collection(BLOGGERS_PLATFORM.blogs);
-  }
-  public get postsCollection() {
-    return this.client
-      .db(BLOGGERS_PLATFORM.dbName)
-      .collection(BLOGGERS_PLATFORM.posts);
-  }
-  public get commentsCollection() {
-    return this.client
-      .db(BLOGGERS_PLATFORM.dbName)
-      .collection(BLOGGERS_PLATFORM.comments);
-  }
-  public get usersCollection() {
-    return this.client
-      .db(BLOGGERS_PLATFORM.dbName)
-      .collection(BLOGGERS_PLATFORM.users);
+export async function runDB() {
+  try {
+    await client.connect();
+
+    const adminDb = client.db().admin();
+    const pingResult = await adminDb.command({ ping: 1 });
+    console.log('Ping successful:', pingResult);
+  } catch (error) {
+    console.log('DB connection failed: ', error);
+    await client.close();
   }
 }
-
-export const getDbUrl = async () => {
-  if (process.env.NODE_ENV === 'test') {
-    const server = await MongoMemoryServer.create();
-    return server.getUri();
-  }
-  return SETTINGS.DB_URL || 'mongodb://0.0.0.0:27017';
-};
-// @ts-ignore
-let bloggers_platform_db: Db;
-let blogsCollection: Collection;
-let postsCollection: Collection;
-let commentsCollection: Collection;
-let usersCollection: Collection;
-
-const db = getDbUrl().then((url) => {
-  const db = new Database(url);
-  bloggers_platform_db = db.bloggers_platform_db;
-  blogsCollection = db.blogsCollection;
-  postsCollection = db.postsCollection;
-  commentsCollection = db.commentsCollection;
-  usersCollection = db.usersCollection;
-});
-
-export {
-  blogsCollection,
-  postsCollection,
-  commentsCollection,
-  usersCollection,
-  bloggers_platform_db,
-};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { app } from './app';
 import SETTINGS from './settings';
-import { Database } from './db/db';
-
+// import { Database } from './db/db';
+import { runDB } from './db/db';
 const PORT = SETTINGS.PORT;
 
-export const indexDb = new Database(SETTINGS.DB_URL || 'mongodb://0.0.0.0:27017');
+// const indexDb = new Database(SETTINGS.DB_URL || 'mongodb://0.0.0.0:27017');
 
 app.listen(PORT, async () => {
-  await indexDb.runDb();
+  // await indexDb.runDb();
+  await runDB();
   console.log(`Server started at ${PORT} port`);
 });
